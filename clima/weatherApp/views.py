6e35e63a6770f234b018e0d5ae2246cc0a244b48
David-Aros <david.aros.c@gmail.com>
2022-06-14 15:06:24 -0400

    se agregan item temp max-min"

--- clima/weatherApp/views.py
+++ clima/weatherApp/views.py
@@ -16,7 +16,8 @@ def index(request):
             "coordinate" : str(list_of_data['coord']['lon']) + ', '
             + str(list_of_data['coord']['lat']),
             "temp" : str(list_of_data['main']['temp']) + 'ºC',
-            
+            "temp_min" : str(list_of_data['main']['temp_min']) + 'ºC',
+            "temp_max" : str(list_of_data['main']['temp_max']) + 'ºC',            
             "pressure" : str(list_of_data['main']['pressure']),
             "humidity" : str(list_of_data['main']['humidity']),
             "main" : str(list_of_data['weather'][0]['main']),
